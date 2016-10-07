13908f9f25087230519e02b28b4110f186380923
Tristan Rice <rice@fn.lc>
2016-10-07 04:00:35 +0000

    a2: Finished 4.3

--- a2/example_animals.m
+++ a2/example_animals.m
@@ -1,12 +1,19 @@
 %% Animals with attributes data
 load animals.mat
 
-%% K-Means clustering
-k = 5;
-model = clusterKmeans(X,k,0);
+%% Density-Based Clustering
+radius = 13;
+minPts = 3;
+doPlot = 0;
+model = clusterDBcluster(X,radius,minPts,doPlot);
 
-for c = 1:k
+%% K-Means clustering
+%k = 5;
+%model = clusterKmeans(X,k,0);
+
+for c = 1:model.k
     fprintf('Cluster %d: ',c);
     fprintf('%s ',animals{model.y==c});
     fprintf('\n');
 end
+
