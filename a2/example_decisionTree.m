2ac003d9620128cbeeb50d1ceaaca45fd71e2518
Tristan Rice <rice@fn.lc>
2016-10-07 00:46:30 +0000

    a2: more work

---
+++ a2/example_decisionTree.m
@@ -0,0 +1,38 @@
+warning('off', 'Octave:divide-by-zero')
+
+% Load X and y variable
+load vowel.mat
+[n,d] = size(X);
+[t,d] = size(Xtest);
+
+depths = 1:15;
+
+training_error = [];
+testing_error = [];
+
+for depth = depths
+  %% Fit decision tree and compute error
+  model = decisionTree(X,y,depth);
+
+  % Evaluate training error
+  yhat = model.predict(model,X);
+  error = sum(yhat ~= y)/n;
+  training_error = [training_error; error];
+  fprintf('Training error with depth-%d decision tree: %.2f\n',depth,error);
+
+  % Evaluate training error
+  yhat = model.predict(model,Xtest);
+  error = sum(yhat ~= ytest)/t;
+  testing_error = [testing_error; error];
+  fprintf('Test error with depth-%d decision tree: %.2f\n',depth,error);
+
+  % Plot classifier
+  %figure(1);
+  %classifier2Dplot(X,y,model);
+end
+
+plot(depths, [training_error testing_error]);
+ylabel('error')
+xlabel('depth')
+legend("Training", "Test")
+print -dpng 2.1.1.png
