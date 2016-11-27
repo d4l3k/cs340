ed2ca0d99696dc31c94019a209a9cca9ce1330f5
Tristan Rice <rice@fn.lc>
2016-11-27 21:37:16 +0000

    a5: finished

--- a5/example_PCA.m
+++ a5/example_PCA.m
@@ -1,12 +1,30 @@
 load animals.mat
 
+k = 5;
+
 [n,d] = size(X);
 X = standardizeCols(X);
 
 figure(1);
 imagesc(X);
 figure(2);
-i = ceil(rand*d);
-j = ceil(rand*d);
-plot(X(:,i),X(:,j),'.');
-gname(animals);
+i = 1
+j = 2
+
+model = dimRedPCA(X,k);
+
+Z = model.compress(model,X);
+
+plot(Z(:,i),Z(:,j),'.');
+
+for k = 1:n
+		text(Z(k,i),Z(k,j),animals(k,:));
+end
+
+print -dpng 1.2.png
+
+k
+d
+Xhat = model.expand(model,Z);
+variance = norm(Xhat-X, "fro")^2/(norm(X, "fro")^2)
+(1-variance)*100
